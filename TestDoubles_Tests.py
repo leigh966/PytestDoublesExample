41cf35c9537f038303f8afbbea723f08e3083328
leigh966 <leigh966@hotmail.com>
2022-03-16 11:15:03 +0000

    Completed red phase for throwsExceptionWithBadFile

--- TestDoubles_Tests.py
+++ TestDoubles_Tests.py
@@ -1,5 +1,5 @@
 from LineReader import readFromFile
-import pytest
+from pytest import *
 from unittest.mock import MagicMock
 
 
@@ -17,3 +17,17 @@ def test_returnsCorrectString(monkeypatch):
     mock_open.assert_called_once_with("blah",'r')
     # Assert that the correct string was returned
     assert result == "test line"
+
+def test_throwsExcetionWithBadFile(monkeypatch):
+    mock_file = MagicMock()
+    # Replace the file.readline within the mock file to return "test line"
+    mock_file.readline = MagicMock(return_value="test line")
+    # Replace the file.open to return our mock file object rather than the actual file
+    mock_open = MagicMock(return_value=mock_file)
+    monkeypatch.setattr("builtins.open", mock_open) # Attach
+
+    mock_exists = MagicMock(return_value=False)
+    monkeypatch.setattr("os.path.exists", mock_exists)
+    with raises(Exception):
+        # Call the method under test
+        result = readFromFile("blah")
